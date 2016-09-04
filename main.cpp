bb9dd8b4843e929305d2d85d3cead039de06b69f
LokoDenis <sanityseeker@ya.ru>
2016-09-04 21:50:44 +0300

    strange ccheck commit

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@
 using namespace cv;
 using namespace cv::xfeatures2d;
 
+int penaltyEqual = 5;  // just an example of values, not tested
+int penaltyDiffer = 100;
+
 template<class T>
 T mymin(std::initializer_list<T> ilist) {  // min function for multiple arguments
     return *std::min_element(ilist.begin(), ilist.end());
@@ -23,10 +26,49 @@ struct Direction {
     Direction(int a = 0, int b = 0): x(a), y(b) {}
 };
 
+cv::Mat censusTransform3x3(const Mat& image) {
+    cv::Mat censusMap(image.size(), CV_8U, Scalar(0));
+    std::vector<const unsigned char*> readLines(3);
+    for (size_t j = 1; j < image.rows - 1; ++j) {
+        for (size_t l = 0; l != 3; ++l) {
+            readLines[l] = image.ptr<unsigned char>(j + l - 1);
+        }
+        unsigned char * censusWriteLine = censusMap.ptr<unsigned char>(j);
+        for (size_t i = 1; i < image.cols - 1; ++i) {
+            int curValue = readLines[1][i];
+            unsigned char censusValue = 0;
+            for (size_t l = 0; l != 3; ++l) {
+                for (size_t k = 0; k != 3; ++k) {
+                    if (k == 0 && l == 0) {
+                        continue;
+                    }
+                    bool less = readLines[l][i + k - 1] < curValue;
+                    censusValue <<= 1;
+                    if (less) censusValue += 1;
+                }
+                censusWriteLine[i] = censusValue;
+            }
+        }
+    }
+    return censusMap;
+}
+
+cv::Mat createDisparityImage(std::vector<std::vector<int>>& disparities, int max, int min) {
+    Mat stereo(disparities[0].size(), disparities.size(), CV_8UC1, Scalar(0));
+    for (size_t x = 0; x != disparities.size(); ++x) {
+        for (size_t y = 0; y != disparities[0].size(); ++y) {
+            stereo.at<uchar> (y, x) = static_cast<uchar>(std::floor(disparities[x][y] * 255 / static_cast<double>(max - min)));
+//            stereo.at<uchar> (y, x) = disparities[x][y];
+        }
+    }
+    medianBlur(stereo, stereo, 3);
+    return stereo;
+}
+
 void leaveBestPoints(std::vector<KeyPoint>& input) {
-    if (input.size() > 100) {
+    if (input.size() > 15) {
         std::sort(input.rbegin(), input.rend(), [](KeyPoint a, KeyPoint b){return a.response * a.size < b.response * b.size;});
-        input.resize(100);
+        input.resize(15);
     }
 }
 
@@ -46,38 +88,20 @@ void measureBorders(Mat& first, Mat& second, int& min_disp, int& max_disp) {  //
     std::vector<DMatch> matches;
     matcher.match(firstDescriptor, secDescriptor, matches);
     
-    double min = 100, max = 0;
-    for (size_t i = 0; i != matches.size(); ++i) {
-        if (matches[i].distance < min) {
-            min = matches[i].distance;
-        }
-        if (matches[i].distance > max) {
-            max = matches[i].distance;
-        }
-    }
-    
-    max_disp = static_cast<int>(ceil(max));
-    min_disp = static_cast<int>(ceil(min));
-}
-
-void countUnary(Mat& first, Mat& second, int max_disparity, std::vector<std::vector<std::vector<int>>>& unaries) {
-    for (size_t y = 0; y != first.rows; ++y) {
-        for (size_t x = 0; x != first.cols; ++x) {
-            for (int d = 0; d <= 2 * max_disparity; ++d) {
-                int currd = d - max_disparity;
-                unaries[x][y][d] = (x + currd >= 0 && x + currd < first.cols ?
-                                   abs(static_cast<int>(first.at<uchar>(y, x)) - static_cast<int>(second.at<uchar>(y, x + currd))) : 0);
-            }
-        }
+    for (size_t i = 0; i != matches.size(); ++i) {  // сравнить по координатам
+       int delta = static_cast<int>(std::ceil(firstKeys[matches[i].queryIdx].pt.x - secKeys[matches[i].trainIdx].pt.x));
+       if (min_disp > delta) {
+           min_disp = delta;
+       }
+       if (max_disp < delta) {
+           max_disp = delta;
+       }
     }
 }
 
-void countDirections(std::vector<std::vector<std::vector<int>>>& unaries, std::vector<std::vector<std::vector<int>>>& temp,
-                     std::vector<std::vector<std::vector<int>>>& aggregated, Direction r, int max_disparity) {
-    
-    double penaltyEqual = 8;  // just an example of values, not tested
-    double penaltyDiffer = 64;
-    
+void countDirections(std::vector<std::vector<std::vector<int>>>& unaries, std::vector<std::vector<std::vector<int>>>& temp
+        , std::vector<std::vector<std::vector<int>>>& aggregated, Direction r
+        , int max_disparity, int min_disparity) {
     int alpha, beta, ksi, eps, c, k;
     
     if (r.x < 0) {
@@ -91,7 +115,7 @@ void countDirections(std::vector<std::vector<std::vector<int>>>& unaries, std::v
     }
     if (r.y < 0) {
         ksi = unaries[0].size() - 1;
-        eps = 0;
+        eps = -1;
         k = -1;
     } else {
         ksi = 0;
@@ -104,13 +128,13 @@ void countDirections(std::vector<std::vector<std::vector<int>>>& unaries, std::v
             int newX = x - r.x;
             int newY = y - r.y;
             if (newX < 0 || newY < 0 || newX >= unaries.size() || newY >= unaries[0].size()) {
-                for (size_t d = 0; d <= 2 * max_disparity; ++d)
+                for (size_t d = 0; d < max_disparity - min_disparity + 1; ++d)
                     temp[x][y][d] = unaries[x][y][d];
             } else {
                 int minimum = *std::min_element(temp[newX][newY].begin(), temp[newX][newY].end());
                 
-                for (size_t d = 0; d <= 2 * max_disparity; ++d) {
-                    if (d == 2 * max_disparity) {
+                for (size_t d = 0; d <= max_disparity - min_disparity + 1; ++d) {
+                    if (d == max_disparity - min_disparity) {
                         temp[x][y][d] = unaries[x][y][d] + static_cast<int>(mymin<double>({temp[newX][newY][d],
                                                                                            temp[newX][newY][d - 1] +
                                                                                            penaltyEqual,
@@ -138,7 +162,7 @@ void countDirections(std::vector<std::vector<std::vector<int>>>& unaries, std::v
     
     for (size_t x = 0; x != aggregated.size(); ++x) {
         for (size_t y = 0; y != aggregated[0].size(); ++y) {
-            for (size_t d = 0; d != 2 * max_disparity; ++d) {
+            for (size_t d = 0; d != max_disparity - min_disparity + 1; ++d) {
                 aggregated[x][y][d] += temp[x][y][d];
             }
         }
@@ -151,43 +175,81 @@ void countDirections(std::vector<std::vector<std::vector<int>>>& unaries, std::v
     }
 }
 
+void countUnary(Mat& first, Mat& second, int max_disparity, int min_disparity, std::vector<std::vector<std::vector<int>>>& unaries) {
+    for (size_t y = 0; y != first.rows; ++y) {
+        for (size_t x = 0; x != first.cols; ++x) {
+            for (int d = 0; d < max_disparity - min_disparity + 1; ++d) {
+                int currd = d + min_disparity;
+                unaries[x][y][d] = (x + currd >= 0 && x + currd < first.cols ?
+                                   abs(static_cast<int>(first.at<uchar>(y, x)) - static_cast<int>(second.at<uchar>(y, x + currd))) : 0);
+            }
+        }
+    }
+}
+
+void countBinary(std::vector<std::vector<std::vector<int>>>& ctable,  std::vector<std::vector<std::vector<int>>>& stable
+                , std::vector<std::vector<std::vector<int>>>& temtable, int max_disp, int min_disp) {
+    
+    Direction up(0, 1), down(0, -1), left(-1, 0), right(1, 0), right_down(1, -1), right_up(1, 1), left_down(-1, -1), left_up(-1, 1),
+            up_diag_left(-2, 1), up_diag_right(2, 1), down_diag_left(-2, -1), down_diag_right(2, -1), diag_right(1, 2), diag_left(-1, 2),
+            diag_down_right(1, -2), diag_down_left(-1, -2);
+    
+    countDirections(ctable, temtable, stable, up, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, down, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, left, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, right, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, right_down, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, right_up, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, left_down, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, left_up, max_disp, min_disp);
+    
+    countDirections(ctable, temtable, stable, up_diag_left, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, up_diag_right, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, down_diag_left, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, down_diag_right, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, diag_right, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, diag_left, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, diag_down_right, max_disp, min_disp);
+    countDirections(ctable, temtable, stable, diag_down_left, max_disp, min_disp);
+}
+
 int main() {  // there will be console flags in final version
     std::string firstSource, secSource;
 //    std::cin >> firstSource >> secSource;
-    firstSource = "/home/sanity-seeker/Programming/stereo_vision/dpth_map_dataset/aloe_left.jpg";
-    secSource = "/home/sanity-seeker/Programming/stereo_vision/dpth_map_dataset/aloe_right.jpg";
-    Mat first = imread(firstSource, CV_LOAD_IMAGE_UNCHANGED);
-    Mat second = imread(secSource, CV_LOAD_IMAGE_UNCHANGED);
+    firstSource = "/home/sanity-seeker/Programming/stereo_vision/dpth_map_dataset/aero/source_left.jpg";
+    secSource = "/home/sanity-seeker/Programming/stereo_vision/dpth_map_dataset/aero/source_right.jpg";
+    Mat first = imread(firstSource, CV_LOAD_IMAGE_GRAYSCALE);
+    Mat second = imread(secSource, CV_LOAD_IMAGE_GRAYSCALE);
+    
+     resize(first, first, Size(600, 800), 0, 0, INTER_LINEAR);
+     resize(second, second, Size(600, 800), 0, 0, INTER_LINEAR);
     
     int min_disp, max_disp;
-    measureBorders(first, second, min_disp, max_disp);
-    max_disp = std::min(3 * min_disp, max_disp); // approximately
+    // measureBorders(first, second, min_disp, max_disp);
+    min_disp = -10;
+    max_disp = 30;
     
-    std::vector<std::vector<std::vector<int>>> ctable(first.cols, (std::vector<std::vector<int>> (first.rows, std::vector<int>(2 * max_disp, 0))));
-    std::vector<std::vector<std::vector<int>>> stable(first.cols, (std::vector<std::vector<int>> (first.rows, std::vector<int>(2 * max_disp, 0))));
-    std::vector<std::vector<std::vector<int>>> temtable(first.cols, (std::vector<std::vector<int>> (first.rows, std::vector<int>(2 * max_disp, 0))));
-    std::vector<std::vector<int>> disparities(first.cols, (std::vector<int>(first.rows, 0)));
+    std::vector<std::vector<std::vector<int>>> ctable(first.cols, (std::vector<std::vector<int>> (first.rows, std::vector<int>(max_disp - min_disp + 1, 0))));
+    std::vector<std::vector<std::vector<int>>> stable(first.cols, (std::vector<std::vector<int>> (first.rows, std::vector<int>(max_disp - min_disp + 1, 0))));
+    std::vector<std::vector<std::vector<int>>> temtable(first.cols, (std::vector<std::vector<int>> (first.rows, std::vector<int>(max_disp - min_disp + 1, 0))));
+    std::vector<std::vector<int>> base_disparities(first.cols, (std::vector<int>(first.rows, 0)));
+    std::vector<std::vector<int>> match_disparities(second.cols, (std::vector<int>(second.rows, 0)));
     // unary potential
-    countUnary(first, second, max_disp, ctable);
+//    Mat censusFirst = censusTransform3x3(first);
+//    Mat censusSecond = censusTransform3x3(second);
+//    countUnary(censusFirst, censusSecond, max_disp, min_disp, ctable);
+    countUnary(first, second, max_disp, min_disp, ctable);
     
     // binary potential
-    Direction up(0, 1), down(0, -1), left(-1, 0), right(1, 0), right_down(1, -1), right_up(1, 1), left_down(-1, -1), left_up(-1, 1);
-    countDirections(ctable, temtable, stable, up, max_disp);
-    countDirections(ctable, temtable, stable, down, max_disp);
-    countDirections(ctable, temtable, stable, left, max_disp);
-    countDirections(ctable, temtable, stable, right, max_disp);
-    countDirections(ctable, temtable, stable, right_down, max_disp);
-    countDirections(ctable, temtable, stable, right_up, max_disp);
-    countDirections(ctable, temtable, stable, left_down, max_disp);
-    countDirections(ctable, temtable, stable, left_up, max_disp);
+    countBinary(ctable, stable, temtable, max_disp, min_disp);
     
-    // select right disparity and create a grayscale image
+    // select right disparity
     int smax = MININT;
     int smin = MAXINT;
     for (size_t x = 0; x != first.cols; ++x) {
         for (size_t y = 0; y != first.rows; ++y) {
             int min_index = std::min_element(stable[x][y].begin(), stable[x][y].end()) - stable[x][y].begin();
-            disparities[x][y] = min_index;
+            base_disparities[x][y] = min_index;
             if (min_index > smax) {
                 smax = min_index;
             }
@@ -196,21 +258,63 @@ int main() {  // there will be console flags in final version
             }
         }
     }
+    Mat base = createDisparityImage(base_disparities, smax, smin);
     
-    Mat stereo(first.rows, first.cols, CV_8UC1, Scalar(0));
+    // doing all calculations assuming match image is a base one
     for (size_t x = 0; x != first.cols; ++x) {
         for (size_t y = 0; y != first.rows; ++y) {
-            stereo.at<uchar> (y, x) = disparities[x][y] * 255 / (smax - smin);
+            std::fill(ctable[x][y].begin(), ctable[x][y].end(), 0);
+            std::fill(stable[x][y].begin(), stable[x][y].end(), 0);
         }
     }
     
-    medianBlur(stereo, stereo, 3);
+    int match_max_disp = std::max(-max_disp, -min_disp);
+    int match_min_disp = std::min(-min_disp, -max_disp);
+    countUnary(second, first, match_max_disp, match_min_disp, ctable);
+    countBinary(ctable, stable, temtable, match_max_disp, match_min_disp);
     
-//    namedWindow("disparity map", CV_WINDOW_AUTOSIZE);
-//    imshow("disparity map", stereo);
-//    waitKey(0);
-//    destroyWindow("disparity map");
+    for (size_t x = 0; x != second.cols; ++x) {
+        for (size_t y = 0; y != second.rows; ++y) {
+            int min_index = std::min_element(stable[x][y].begin(), stable[x][y].end()) - stable[x][y].begin();
+            match_disparities[x][y] = min_index;
+        }
+    }
+    Mat match = createDisparityImage(match_disparities, smax, smin);
     
-    imwrite("/home/sanity-seeker/Programming/stereo_vision/results/aloe5.jpg", stereo);
+    for (size_t y = 0; y != match.rows; ++y) {
+        for (size_t x = 0 ; x != match.cols; ++x) {
+            match.at<uchar>(y, x) = 255 - match.at<uchar>(y, x);
+        }
+    }
+    
+    // calculating final disparity image
+    Mat disparity(first.size(), CV_8UC1, Scalar(0));
+    
+    int invalid = 100;
+    
+    for (size_t x = 0; x != first.cols; ++x) {
+        for (size_t y = 0; y != first.rows; ++y) {
+            if (!(x + base_disparities[x][y] - min_disp < 0 || x + base_disparities[x][y] - min_disp >= first.cols)) {
+                if (abs(base.at<uchar>(y, x) - match.at<uchar>(y, x + base_disparities[x][y] - min_disp)) <= 30) {
+                    disparity.at<uchar>(y, x) = base.at<uchar>(y, x);
+                } else {
+                    disparity.at<uchar>(y, x) = invalid;
+                }
+            }
+        }
+    }
+    //medianBlur(disparity, disparity, 3);
+    
+    namedWindow("base", CV_WINDOW_AUTOSIZE);
+    namedWindow("match", CV_WINDOW_AUTOSIZE);
+    namedWindow("final", CV_WINDOW_AUTOSIZE);
+    imshow("match", match);
+    imshow("base", base);
+    imshow("final", disparity);
+    waitKey(0);
+    destroyAllWindows();
+    
+//    imwrite("/home/sanity-seeker/Programming/stereo_vision/results/aero/source++_" + std::to_string(min_disp) + '_' + std::to_string(max_disp) + "_" +
+//            std::to_string(penaltyEqual) + "_" + std::to_string(penaltyDiffer) + ".jpg", stereo);
     return 0;
 }
